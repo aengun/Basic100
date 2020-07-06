6954b815f02009b4113ca9f975c1249a19cf95b7
aengun <aengun@icloud.com>
2020-07-06 14:01:01 +0900

    0706

---
+++ src/ps001.java
@@ -0,0 +1,10 @@
+
+public class ps001 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello");
+
+	}
+
+}
---
+++ src/ps002.java
@@ -0,0 +1,8 @@
+
+public class ps002 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
